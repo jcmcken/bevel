1d57902e7099b5ce1281506fe6b457d6e91f8586
Jon McKenzie <jcmcken@gmail.com>
2014-02-08 15:21:17 -0500

    Fill out many more features (including internal changes)

--- bevel/__init__.py
+++ bevel/__init__.py
@@ -6,6 +6,16 @@ import re
 import shlex
 import logging
 import time
+import subprocess
+import errno
+import pprint
+
+json = None
+for lib in ['json', 'simplejson']:
+    try:
+        json = __import__(lib)
+    except ImportError:
+        pass
 
 LOG = logging.getLogger('bevel')
 
@@ -14,18 +24,21 @@ RE_VALID_COMMAND = re.compile(_RE_VALID_COMMAND)
 
 class InvalidCommand(RuntimeError): pass
 class InvalidBevel(ValueError): pass
+class InternalError(RuntimeError): pass
 
 class Bevel(object):
     DRIVER_NAME = '_driver'
 
     def __init__(self, bin_dir):
         self.bin_dir = bin_dir.rstrip('/')
-        self.app_name = os.path.basename(self.bin_dir)
-        if not self._is_valid_name(self.app_name):
+        self.name = os.path.basename(self.bin_dir)
+        if not self._is_valid_name(self.name):
             raise InvalidBevel(self.bin_dir) 
 
     def _args_to_path(self, args):
-        return os.path.join(self.bin_dir, os.path.sep.join(args)).rstrip('/')
+        result = os.path.join(self.bin_dir, os.path.sep.join(args)).rstrip('/')
+        LOG.debug("args %s corresponds to path '%s'" % (args, result))
+        return result
 
     def _get_bin(self, path):
         bin = None
@@ -35,13 +48,19 @@ class Bevel(object):
             bin = path
         if bin and not os.path.isfile(bin):
             bin = None
+        LOG.debug("command path '%s' corresponds to script '%s'" % (path, bin))
         return bin
 
     def _is_valid_name(self, command):
         # Is ``command`` a valid command name?
-        return bool(RE_VALID_COMMAND.match(command))
+        result = bool(RE_VALID_COMMAND.match(command))
+        LOG.debug("'%s' is a valid command name? %s" % (command, result))
+        return result
 
     def _args_are_valid(self, args):
+        """
+        Whether all ``args`` have valid command names
+        """
         return all(map(self._is_valid_name, args))
 
     def _subcommands(self, args):
@@ -50,56 +69,126 @@ class Bevel(object):
             return []
 
         basedir = os.path.dirname(bin)
-        return [ i for i in os.listdir(basedir) if self._appears_as_command(os.path.join(basedir, i)) ]
+        result = [ i for i in os.listdir(basedir) if self._appears_as_command(os.path.join(basedir, i)) ]
+        LOG.debug("subcommands for %s are %s" % (args, result))
+        return result
 
     def _appears_as_command(self, path):
+        """
+        Whether a particular ``path`` appears as a command/subcommand
+        """
         return self._is_driver_command(path) or self._is_regular_command(path) 
 
     def _has_driver(self, path):
-        return os.path.isfile(os.path.join(path, self.DRIVER_NAME))
+        """
+        Whether a particular command directory has a driver file
+        """
+        result = os.path.isfile(os.path.join(path, self.DRIVER_NAME))
+        LOG.debug("path '%s' has a driver? %s" % (path, result))
+        return result
+
+    def _is_runnable(self, path):
+        """
+        Whether a particular path is a runnable script
+        """
+        runnable = os.access(path, os.R_OK|os.X_OK) and os.path.isfile(path)
+        LOG.debug("script '%s' is runnable? %s" % (path, runnable))
+        return runnable
 
     def _is_regular_command(self, bin):
-        return not self._is_driver_file(bin) and os.path.isfile(bin) and \
+        result = not self._is_driver_file(bin) and os.path.isfile(bin) and \
             self._is_valid_name(os.path.basename(bin)) and \
-            self._has_driver(os.path.dirname(bin))
+            self._has_driver(os.path.dirname(bin)) and self._is_runnable(bin)
+        LOG.debug("'%s' is a leaf command? %s" % (bin, result))
+        return result
 
     def _is_driver_command(self, path):
-        return self._has_driver(path) and self._is_valid_name(os.path.basename(path))
+        result = self._has_driver(path) and self._is_valid_name(os.path.basename(path))
+        LOG.debug("'%s' is a parent command? %s" % (path, result))
+        return result
         
     def _is_driver_file(self, bin):
-        return bin.endswith(os.path.sep + self.DRIVER_NAME) 
+        """
+        Whether a particular path is command driver
+        """
+        result = bin.endswith(os.path.sep + self.DRIVER_NAME) and \
+            self._is_runnable(bin) 
+        LOG.debug("'%s' is a driver script? %s" % (bin, result))
+        return result
 
     def _args_to_bin(self, args):
+        """
+        Convert command-line arguments to a script path
+        """
         result = None
         if self._args_are_valid(args):
             result = self._get_bin(self._args_to_path(args))
+        if result is not None and not self._is_runnable(result):
+            result = None
+        LOG.debug("converted args %s to valid script '%s'" % (args, result))
         return result
 
     def _resolve_args(self, args):
+        """
+        Resolve the next available subcommand from command-line arguments.
+        
+        E.g. if ``baz`` is an invalid subcommand to ``foo bar``, then resolve
+        ``foo bar`` so that you can automatically call usage 
+        """
         lookup_args = copy.copy(args)
         remainder = []
         bin = None
         while lookup_args:
-            bin = self._args_to_bin(lookup_args)
-            if bin is not None:
+            candidate = self._args_to_bin(lookup_args)
+            LOG.debug('candidate script for %s is "%s"' % (args, candidate))
+            if candidate is not None:
+                LOG.debug('selecting candidate "%s"' % candidate)
+                bin = candidate
                 break
+            LOG.debug('skipping candidate "%s"' % candidate)
             remainder.append(lookup_args.pop(-1))
         if bin is None:
             bin = self._args_to_bin(lookup_args)
-        return bin, remainder
+        # remainder args were populated backwards, so reverse them
+        remainder.reverse()
+        results = (bin, remainder)
+        LOG.debug('args %s resolve to script path "%s" with remainder args %s' % (args, results[0], results[1]))
+        return results
 
     def _parse_args(self, args):
-        return shlex.split(args)
+        """
+        Convert string arguments to an array
+        """
+        parsed = shlex.split(args)
+        LOG.debug('parsed "%s" as %s' % (args, parsed))
+        return parsed
 
     def _run(self, script, args):
+        """
+        Run ``script`` with arguments ``args``
+        """
         LOG.info("running script '%s' with args %s" % (script, args))
         now = time.time()
-        # run it
+        full_args = [script] + args
+        run = False
+        try:
+            subprocess.Popen(full_args)
+            run = True
+        except OSError, e:
+            if e.errno == errno.ENOEXEC:
+                raise InternalError('could not determine subcommand runtime')
+            else: raise
+                
         LOG.info("command finished in %3f seconds" % (time.time() - now))
 
-    def run(self, args):
+    def run(self, args, noop=False):
         bin, remainder_args = self._resolve_args(self._parse_args(args))
-        self._run(bin, remainder_args)
+        # TODO: don't call bin with remainder args if command resolves
+        #       fuzzily; just call ``self._run(bin)`` 
+        if bin is None:
+            raise InternalError('could not resolve any valid, runnable scripts')
+        if not noop:
+            self._run(bin, remainder_args)
 
     def _complete(self, args):
         subcommands = self._subcommands(args)
@@ -112,18 +201,52 @@ class Bevel(object):
     def complete(self, args):
         return self._complete(self._parse_args(args))
 
+    def _verify(self):
+        bad = {
+          'permissions': {
+            'dirs': [],
+            'files': [],
+          }
+        }
+        for basedir, dirs, files in os.walk(self.bin_dir):
+            for dir in dirs:
+                fullname = os.path.join(basedir, dir)
+                if not os.access(fullname, os.R_OK|os.X_OK):
+                    bad['permissions']['dirs'].append(fullname)
+            for file in files:
+                fullname = os.path.join(basedir, file)
+                if not self._is_runnable(fullname):
+                    bad['permissions']['files'].append(fullname)
+        return bad
+
+    def verify(self):
+        data = self._verify()
+        if json:
+            result = json.dumps(data, indent=2)
+        else:
+            result = pprint.pformat(data)
+        return result
+
+def enable_debug():
+    logging.basicConfig()
+    LOG.setLevel(logging.DEBUG)
+
 def create_cli():
-    cli = optparse.OptionParser()
+    cli = optparse.OptionParser(prog='bevel')
     cli.add_option('-a', '--args', default="",
         help="Arguments as passed from your CLI application")
     cli.add_option('-b', '--bindir',
         help="The directory location of your `bevel' scripts")
-    cli.add_option('-V', '--verify', action='store_true',
-        help="Verify that your `bevel' commands are properly set up and configured")
     cli.add_option('-c', '--complete', action='store_true',
-        help="Instead of running your `bevel' app, just autocomplete the last subcommand")
+        help="Instead of running your `bevel' app, just autocomplete the last subcommand.")
+    cli.add_option('-n', '--noop', action='store_true',
+        help="Do everything normally, except don't run any scripts.")
     cli.add_option('-d', '--debug', action='store_true',
         help="Print debug messages to stdout")
+    cli.add_option('-V', '--verify', action='store_true',
+        help="Verify that your `bevel' commands are properly set up and configured."
+             " This option is for development purposes. Returns a data structure "
+             "containing all of the directories or files that may be incorrect.")
     return cli
 
 def main(argv=None):
@@ -131,8 +254,7 @@ def main(argv=None):
     opts, args = cli.parse_args(argv)
 
     if opts.debug:
-        logging.basicConfig()
-        LOG.setLevel(logging.DEBUG)
+        enable_debug()
 
     if not opts.bindir:
         cli.error('must pass bin directory (-b/--bindir)')
@@ -143,7 +265,7 @@ def main(argv=None):
     app = Bevel(opts.bindir)
 
     if opts.verify:
-        app.verify()
+        print app.verify()
         raise SystemExit
 
     if opts.complete:
@@ -151,7 +273,11 @@ def main(argv=None):
         print '\n'.join(completion)
         raise SystemExit
 
-    app.run(opts.args)
+    try:
+        app.run(opts.args, noop=opts.noop)
+    except InternalError, e:
+        sys.stderr.write("%s: internal error: %s\n" % (app.name, e.args[0]))
+        raise SystemExit(1)
 
 if __name__ == '__main__':
     main()
